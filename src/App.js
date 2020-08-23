4163cef119345f92a565ea5140ded21ca92632c7
DijanaDixi <dijana.1986@live.com>
2020-08-23 18:16:05 +0200

    eslint_disable

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   const [users, setUsers] = useState([]);
   // grid or list
   const [showUsers, setUsersView] = useState(true);
+   // eslint-disable-next-line
   const [count, setCount] = useState(20);
   // loader
   const [loader, isLoading] = useState(true);
